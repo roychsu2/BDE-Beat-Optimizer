5b36764f61fc2b4d5e563e387db5ab8f42f5c368
MedzOnERP <medzonerp@gmail.com>
2026-07-15 00:31:06 +0530

    Use user-provided updated logo.png

--- make_trans.ps1
+++
@@ -1,6 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bmp = New-Object System.Drawing.Bitmap("logo.png")
-$bgColor = $bmp.GetPixel(0, 0)
-$bmp.MakeTransparent($bgColor)
-$bmp.Save("logo_transparent.png", [System.Drawing.Imaging.ImageFormat]::Png)
-$bmp.Dispose()
